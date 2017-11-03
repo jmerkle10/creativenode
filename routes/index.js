75bd0473fa17130a16315446cd01c28d47aa6727
corbittcoder <scttcorbitt@gmail.com>
2017-11-03 15:35:21 +0000

    fixed the file structure so the damn thing made sense

--- routes/index.js
+++ routes/index.js
@@ -6,30 +6,22 @@ var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-res.sendFile('weather.html',{root: 'public'});
+  res.sendFile('weather.html',{root: 'public'});
 })
-router.get('/getcity',function(req,res,next) {
-    var myRe = new RegExp("^" + req.query.q);
-       console.log(myRe);
-   fs.readFile(__dirname + '/cities.dat.txt',function(err,data)
-    {
-if(err) throw err;
-var cities = data.toString().split('\n');
-console.log(cities);
-var jsonresult = [];
-	for(var i = 0; i < cities.length;i++){
-	var result = cities[i].search(myRe);
-	if(result != -1){
-	console.log(cities[i]);
-	jsonresult.push({city:cities[i]});
-	}
-	}
-	res.status(200).json(jsonresult);
 
+
+
+router.get('/imageout',function(req,res,next){
+     var k = "http://api.wolframalpha.com/v1/simple?appid=KGYWVU-VGRJV3G3U4&i=";
+    k += req.query.q;
+    console.log("request received");
+    request(k).pipe(res);
 });
 
-  });
-
-
+var word = "https://owlbot.info/api/v1/dictionary/";
+router.get('/word', function(req,res) {
+  word += req.query.q;
+  request(word).pipe(res);
+});
 
 module.exports = router;
