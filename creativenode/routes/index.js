17dc53f4f67591bb0d1bd8c84a85a6e2ce56187e
Joshua <merkleyjosh@gmail.com>
2017-11-03 05:48:51 +0000

    The Backend Works!

--- creativenode/routes/index.js
+++ creativenode/routes/index.js
@@ -11,11 +11,11 @@ res.sendFile('weather.html',{root: 'public'});
 
 
 
-router.get('/word',function(req,res,next){
-     var owl = "https://owlbot.info/api/v1/dictionary/";
-    owl += req.query.q;
-    console.log("in getword");
-    request(owl).pipe(res);
+router.get('/imageout',function(req,res,next){
+     var k = "http://api.wolframalpha.com/v1/simple?appid=KGYWVU-VGRJV3G3U4&i=";
+    k += req.query.q;
+    console.log("request received");
+    request(k).pipe(res);
 });
 
 
