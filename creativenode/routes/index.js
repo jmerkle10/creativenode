72ac8a07d410af9a0a9be211fadc6683766ceb89
Joshua <merkleyjosh@gmail.com>
2017-11-03 04:47:56 +0000

    ommit with updated routes and frontend with just animal query

--- creativenode/routes/index.js
+++ creativenode/routes/index.js
@@ -8,27 +8,8 @@ var request = require('request');
 router.get('/', function(req, res, next) {
 res.sendFile('weather.html',{root: 'public'});
 })
-router.get('/getcity',function(req,res,next) {
-    var myRe = new RegExp("^" + req.query.q);
-       console.log(myRe);
-   fs.readFile(__dirname + '/cities.dat.txt',function(err,data)
-    {
-if(err) throw err;
-var cities = data.toString().split('\n');
-console.log(cities);
-var jsonresult = [];
-	for(var i = 0; i < cities.length;i++){
-	var result = cities[i].search(myRe);
-	if(result != -1){
-	console.log(cities[i]);
-	jsonresult.push({city:cities[i]});
-	}	
-	}	
-	res.status(200).json(jsonresult);
 
-});
 
-  });
 
 router.get('/word',function(req,res,next){
      var owl = "https://owlbot.info/api/v1/dictionary/";
