3c8e12b39671348cee0e788fc96639c0bac9157c
corbittcoder <scttcorbitt@gmail.com>
2017-11-03 19:52:53 +0000

    now working with queries

--- routes/index.js
+++ routes/index.js
@@ -9,19 +9,11 @@ router.get('/', function(req, res, next) {
   res.sendFile('weather.html',{root: 'public'});
 })
 
-
-
 router.get('/imageout',function(req,res,next){
      var k = "http://api.wolframalpha.com/v1/simple?appid=KGYWVU-VGRJV3G3U4&i=";
     k += req.query.q;
     console.log("request received");
     request(k).pipe(res);
-});
-
-var word = "https://owlbot.info/api/v1/dictionary/";
-router.get('/word', function(req,res) {
-  word += req.query.q;
-  request(word).pipe(res);
 });
 
 module.exports = router;
